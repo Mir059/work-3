c441a47fbf809714353e70c0c4b6a3772cabe9d0
Mir <Miroslav.isakov.05@inbox.ru>
2025-04-13 12:11:23 +0500

    ruby

--- route.rb
+++ route.rb
@@ -1,19 +1,18 @@
 class Route
   attr_reader :stations
-
   def initialize(first_station, last_station)
-      @stations = [first_station, last_station]
+    @stations = [first_station, last_station]
   end
     
   def add(station)
-      @stations.insert(1,station)
+    @stations.insert(1,station)
   end
 
   def delete(station) 
-      @stations.delete(station)
+    @stations.delete(station)
   end
 
   def list 
-      @stations.each { |s| puts s }
+    @stations.each { |s| puts s }
   end
 end
